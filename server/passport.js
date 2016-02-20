7407344043aac886fcce3e65099fe7fa263446db
AlinaBakalova <bakalova.alina@gmail.com>
2016-02-20 18:04:00 +0200

    passport

--- server/passport.js
+++ server/passport.js
@@ -18,4 +18,17 @@ module.exports = function(app, passport) {
 			return done(null, user);
 		});
 	}));
+	passport.use('local-signup', new LocalStrategy({
+		usernameField : 'username',
+		passwordField : 'password',
+		passReqToCallback : true
+	},
+	function(req, username, password, done) {
+		User.findOne({
+			'username':username
+		},function(err, user) {
+			if (err) return done(err);
+			if (user) return done(null, false);
+		});
+	}));
 };
