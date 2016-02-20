84aaa3559ba59db1b003f68acbfad07511be4e18
Honchar Denys <crackeraki@gmail.com>
2016-02-20 17:57:13 +0200

    fixed user.js on the server

--- server/user.js
+++ server/user.js
@@ -2,22 +2,18 @@ var User = require('./databases/User');
 var api = '/api/user';
 
 module.exports = function(app, passport, express) {
-	var router = express.Router();
-
-	router.get('/logout', function (req, res) {
+	app.get('/logout', function (req, res) {
 		req.logout();
 		res.redirect('/');
 	});
-	router.post('/login', passport.authenticate('local-login', {
+	app.post('/login', passport.authenticate('local-login', {
 		successRedirect: '/',
 		failureRedirect: '/'
 	}));
-	router.post('/signup', passport.authenticate('local-signup', {
+	app.post('/signup', passport.authenticate('local-signup', {
 		successRedirect: '/',
 		failureRedirect: '/'
 	}));
-
-	app.use(api, router);
 };
 // } ensure user login{
 	function isLoggedIn(req, res, next) {
