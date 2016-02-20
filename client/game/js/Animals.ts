2eb6a9fcf3c43b594ab14ac8a837b4ea612cb177
Natali <natalimarynina@gmail.com>
2016-02-20 20:40:02 +0200

    ImageService

--- client/game/js/Animals.ts
+++ client/game/js/Animals.ts
@@ -1,18 +1,26 @@
 import {Component, OnInit} from 'angular2/core';
 import {Router} from 'angular2/router';
+import {Image, ImageService}   from '/game/js/ImageService.ts';
 
 @Component({
 	templateUrl: `/game/html/Animals.html`,
+	bindings: [ImageService]
 })
 export class Animals implements OnInit{
+
 	constructor(
-		private _router: Router){
+		private _router: Router,
+		private _service: ImageService){
 
 	}
 	onSelect() {
 		this._router.navigate(['Garden']);
 	}
+	console.log(Image);
+	images: Image[];
 
-	images: Images[];
+	ngOnInit() {
+	   this._service.getImages().then(images => this.images = images)
+	 }
 
 }
