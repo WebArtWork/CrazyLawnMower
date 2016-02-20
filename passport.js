10f7ad8b6aa5f3962b648989f68c223a3d3d0253
AlinaBakalova <bakalova.alina@gmail.com>
2016-02-20 17:52:13 +0200

    merge

--- passport.js
+++ passport.js
@@ -1,3 +1,4 @@
+var LocalStrategy = require('passport-local').Strategy;
 var User = require('./databases/User');
 
 module.exports = function(app, passport) {
@@ -15,28 +16,6 @@ module.exports = function(app, passport) {
 			if (!user) return done(null, false);
 			if (!user.validPassword(password)) return done(null, false);
 			return done(null, user);
-		});
-	}));
-	passport.use('local-signup', new LocalStrategy({
-		usernameField : 'username',
-		passwordField : 'password',
-		passReqToCallback : true
-	},
-	function(req, username, password, done) {
-		User.findOne({
-			'username':username
-		},function(err, user) {
-			if (err) return done(err);
-			if (user) return done(null, false);
-					var newUser = new User();
-				newUser.username= username;
-				newUser.password= newUser.generateHash(password);
-				newUser.profileUrl= 'id'+Date.now();
-				newUser.save(function(err) {
-					if (err) throw err;
-					return done(null, newUser);
-				});
-			}
 		});
 	}));
 };
