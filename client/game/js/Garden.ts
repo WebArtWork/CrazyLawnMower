a8fcbd47b97441f727d4329af4cb5ad69b33e9ac
Honchar Denys <crackeraki@gmail.com>
2016-02-20 19:52:10 +0200

    garden part one

--- client/game/js/Garden.ts
+++ client/game/js/Garden.ts
@@ -5,6 +5,9 @@ import {Router} from 'angular2/router';
 	templateUrl: `/game/html/Garden.html`,
 })
 export class Garden implements OnInit {
+	grass: number = 0;
+	timeout: string;
+	counter: number = 0;
 
 	constructor(private _router: Router){
 
@@ -13,9 +16,24 @@ export class Garden implements OnInit {
 		this._router.navigate(['Animals']);
 	}
 	onSelectGrass() {
+		console.log('this.counter');
+		console.log(this.counter);
+		if(++this.counter==10){
+			this._router.navigate(['Animals']);
+		}else{
+			this.grass=0;
+			clearTimeout(this.timeout);
+			this.levelUpGrass();
+		}
 		console.log('onSelectGrass');
 	}
 	ngOnInit() {
 		console.log('init called');
+		this.levelUpGrass();
+	}
+	levelUpGrass(){
+		this.timeout = setTimeout(function(){
+			if(++this.grass<5) this.levelUpGrass();
+		}, 1000);
 	}
 }
