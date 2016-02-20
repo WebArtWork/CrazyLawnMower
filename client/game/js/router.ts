fb839751d94f9fff06c5c8b6405cd3f2706d0566
Honchar Denys <crackeraki@gmail.com>
2016-02-20 17:03:11 +0200

    cleaned ng2 code

--- client/game/js/router.ts
+++ client/game/js/router.ts
@@ -4,23 +4,15 @@ import {Garden}     from '/game/js/Garden.ts';
 import {Animals}     from '/game/js/Animals.ts';
 
 @Component({
-  selector: 'my-app',
-  template: `
-    <h1 class="title">Component Router</h1>
-    <nav>
-      <a [routerLink]="['Animals']">Animals</a>
-      <a [routerLink]="['Garden']">Garden</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
-  directives: [ROUTER_DIRECTIVES]
+	selector: 'my-app',
+	template: `<router-outlet></router-outlet>
+	`,
+	directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-  {path: '/Garden',   name: 'Garden',     component: Garden},
-  {path: '/Animals',   name: 'Animals',     component: Animals},
+	{path: '/', name: 'Garden', component: Garden},
+	{path: '/Animals', name: 'Animals', component: Animals},
 ])
 export class Router {
-	// constructor(private _router: Router,routeParams: RouteParams){
-	// 	this._router.navigate(['CrisisCenter']);
-	// }
+
 }
